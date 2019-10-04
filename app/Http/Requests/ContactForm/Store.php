d32cac7daa2f72ed3c30487887289a4686e0d4e8
Muhammad Rizky <mrizky98@gmail.com>
2019-10-04 14:37:55 +0700

    validasi form misi 1

---
+++ app/Http/Requests/ContactForm/Store.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests\ContactForm;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class Store extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+          return [
+              'name' => ['required', new \App\Rules\MinimumWords(2)],
+              'email' => ['required', 'email','regex:/(.*)go\.id|ac\.id|or\.id$/i'],
+              'message' => ['required', 'min:20'],
+        ];
+    }
+}
--- resources/views/contact-form/create.blade.php
+++ resources/views/contact-form/create.blade.php
@@ -2,9 +2,9 @@
 @section('content')
     <h1 class="ui header dividing m-b-2">Apa yang bisa kami bantu?</h1>
     {!! form()->post(route('contact-form.create')) !!}
-    {!! form()->text('name')->label('Nama Lengkap') !!}
-    {!! form()->email('email')->label('Alamat Email') !!}
-    {!! form()->textarea('message')->label('Pesan') !!}
+    {!! form()->text('name')->label('Nama Lengkap')->required() !!}
+    {!! form()->email('email')->label('Alamat Email')->required() !!}
+    {!! form()->textarea('message')->label('Pesan')->required() !!}
     {!! form()->submit('Kirim') !!}
     {!! form()->close() !!}
 @stop
